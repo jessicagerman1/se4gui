87fd64b725c0b4b0c944f9ff631058dd7e595877
jessicagerman1 <j.german2222@student.leedsbeckett.ac.uk>
2023-04-23 23:50:31 +0100

    adding initial start to allow text input from user with driver class

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+	public class Driver {
+	    public static void main(String[] args){
+	        Frame frame = new Frame();
+	        frame.setTitle("Assignment 2");
+	        frame.setSize(600,600);
+	        frame.setLocationRelativeTo(null);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setVisible(true); // have to set frames' visibility to true in order to see them
+	    }
+	}
---
+++ src/TextPanel.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TextPanel extends JPanel {
+    private String str;
+    private JTextArea textArea;// makes it an attribute of the class so can use in multiple methods
+    public TextPanel() {
+        textArea = new JTextArea();
+        textArea.setEditable(true);
+        textArea.setLineWrap(true);
+        JScrollPane scroller = new JScrollPane(textArea);// allows scrolling function in the text panel
+        scroller.setPreferredSize(new Dimension(300, 300));
+        add(scroller);
+    }
+
+    public String[] getInstructions() {
+        return str.split("\n");//splits by each line as each command is on a new line, returns separate commands
+    }
+
+    public void updateTextArea(String[] instructions) {
+        str = "";
+        for (String s: instructions) { //adds all lines in instructions string array to str value
+            str+= s +"\n"; //goes through each line
+        }
+        textArea.setText(str);//sets text area equal to string
+    }
+}
